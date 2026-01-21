3709492fec1ed0b8b8224ec0c4ca919cdaebdd7e
baraka24 <bkinywa24@gmail.com>
2026-01-21 18:20:24 +0200

    Task 2. Styling with NativeWind (TailwindCSS)

--- prodev-mobile-app-0x06/tailwind.config.js
+++ prodev-mobile-app-0x06/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#34967C',
+        secondary: '#0601B4',
+      },
+    },
   },
   plugins: [],
 }
